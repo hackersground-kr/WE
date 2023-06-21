beffdc916138e5f47e3d968f9100202b13885db1
Yeo Dayeong <87405998+Yodayeong@users.noreply.github.com>
2023-06-21 15:58:24 +0900

    [Add] Like 도메인 생성

--- Back/src/main/java/HackerGround/WEIN/domain/like/Like.java
+++ Back/src/main/java/HackerGround/WEIN/domain/like/Like.java
@@ -1,4 +1,4 @@
-package HackerGround.WEIN.domain.comment;
+package HackerGround.WEIN.domain.like;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,22 +8,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class Comment {
+public class Like {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+//    foreign key
     private Long boardId;
     private Long userId;
 
-    private String title;
-    private String description;
-    private LocalDateTime createdTime;
+    private Long count;
 }
