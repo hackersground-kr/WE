4926e125568966d16f2bf103ef05cd1d80fa3d21
Yeo Dayeong <87405998+Yodayeong@users.noreply.github.com>
2023-06-21 15:54:35 +0900

    [Add] comment 도메인 생성

---
+++ Back/src/main/java/HackerGround/WEIN/domain/comment/Comment.java
@@ -0,0 +1,29 @@
+package HackerGround.WEIN.domain.comment;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private Long boardId;
+    private Long userId;
+
+    private String title;
+    private String description;
+    private LocalDateTime createdTime;
+}
