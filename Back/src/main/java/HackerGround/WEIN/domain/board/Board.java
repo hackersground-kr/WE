88a47879581327f843306f81f5a7a650efef9b30
gytjd <hys339631@gmail.com>
2023-06-21 15:23:55 +0900

    [INIT] : 개발환경생성

---
+++ Back/src/main/java/HackerGround/WEIN/domain/board/Board.java
@@ -0,0 +1,16 @@
+package HackerGround.WEIN.domain.board;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Board {
+    
+
+}
