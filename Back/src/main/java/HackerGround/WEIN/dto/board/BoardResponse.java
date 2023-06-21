ac3d899466d0d21330705649353a8128c4af1dc5
Yeo Dayeong <87405998+Yodayeong@users.noreply.github.com>
2023-06-22 01:08:13 +0900

    [init] 개발환경 재설정

--- Back/src/main/java/HackerGround/WEIN/dto/board/BoardResponse.java
+++
@@ -1,4 +0,0 @@
-package HackerGround.WEIN.dto.board;
-
-public class BoardResponse {
-}
--- Back/src/main/java/HackerGround/WEIN/repository/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package HackerGround.WEIN.repository;
-
-import HackerGround.WEIN.domain.comment.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-}
