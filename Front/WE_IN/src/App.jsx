ac77f5ac1f3b24796847c543346440cae47f56f8
KimKyuHoi <k546kh@gmail.com>
2023-06-21 22:20:49 +0900

    Feat: FooterMenu 생성

--- Front/WE_IN/src/App.jsx
+++ Front/WE_IN/src/App.jsx
@@ -3,19 +3,25 @@ import { Routes,Route } from 'react-router'
 // import Login from './pages/Login'
 import Main from "./Components/pages/MainPage/Main"
 import './App.css'
-
+import { createTheme, ThemeProvider } from "@mui/material";
 
 function App() {
 
+  
+  const theme = createTheme({
+    typography:{
+      fontFamily: 'Pretendard-Regular'
+    }
+  })
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Routes>
         {/* <Route path='/Login' element={<Login />} />
         <Route path='/Logout' element={<Logout />}/> */}
-        <Route path='/Main' element={<Main />} />
+        <Route path='/' element={<Main />} />
       </Routes>
-    </>
+    </ThemeProvider>
   )
 }
 
