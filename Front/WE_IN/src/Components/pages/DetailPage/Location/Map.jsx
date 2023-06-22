dd5cdd7a40f7f15bcb0eecc9db05e996e4dd89f9
hwiiy <87405971+hwiiy@users.noreply.github.com>
2023-06-22 13:18:48 +0900

    feat : Map.jsx overlay 완료

--- Front/WE_IN/src/Components/pages/DetailPage/Location/Map.jsx
+++ Front/WE_IN/src/Components/pages/DetailPage/Location/Map.jsx
@@ -2,58 +2,84 @@ import { useEffect } from "react";
 import styled from "styled-components";
 import Header from "../../../Layout/Header";
 
-const { kakao } = window;
-
 function KakaoMap() {
   useEffect(() => {
     const mapscript = () => {
-      let container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+      let container = document.getElementById("map");
 
       navigator.geolocation.getCurrentPosition(
         (position) => {
           let { latitude, longitude } = position.coords;
 
           let options = {
-            //지도를 생성할 때 필요한 기본 옵션
-            center: new kakao.maps.LatLng(latitude, longitude), //지도의 중심좌표. 현재 위치로 설정됩니다.
-            level: 3, //지도의 레벨(확대, 축소 정도)
+            center: new kakao.maps.LatLng(latitude, longitude),
+            level: 4,
           };
-          const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
+          const map = new kakao.maps.Map(container, options);
 
-          const markerData = [
+          const overlayData = [
             {
-              title: "place1",
+              title: "수묵 담채화 드로잉",
               lat: latitude + 0.001,
               lng: longitude + 0.001,
             },
             {
-              title: "place2",
-              lat: latitude + 0.0025,
-              lng: longitude + 0.0025,
+              title: "전통 음식 클래스",
+              lat: latitude + 0.003,
+              lng: longitude + 0.003,
             },
             {
-              title: "place3",
+              title: "논어 스토리텔링",
               lat: latitude - 0.0001,
               lng: longitude - 0.0001,
             },
             {
-              title: "place4",
-              lat: latitude - 0.0005,
-              lng: longitude - 0.0005,
+              title: "색소폰 연주",
+              lat: latitude - 0.005,
+              lng: longitude - 0.005,
             },
             {
-              title: "place5",
-              lat: latitude - 0.0025,
-              lng: longitude - 0.0025,
+              title: "통기타 연주 클래스",
+              lat: latitude - 0.003,
+              lng: longitude - 0.003,
             },
           ];
+          const imageSrc = "/logo.png";
+          const imageSize = new kakao.maps.Size(64, 69);
+          const imageOption = { offset: new kakao.maps.Point(27, 69) };
 
-          markerData.forEach((el) => {
-            // marker 생성
-            new kakao.maps.Marker({
+          overlayData.forEach((data) => {
+            const { title, lat, lng } = data;
+
+            const markerImage = new kakao.maps.MarkerImage(
+              imageSrc,
+              imageSize,
+              imageOption
+            );
+            const markerPosition = new kakao.maps.LatLng(lat, lng);
+
+            const marker = new kakao.maps.Marker({
+              position: markerPosition,
+              image: markerImage,
+            });
+
+            marker.setMap(map);
+
+            const content = `
+              <div style="padding: 5px 10px; border-radius:20%; margin-top:20px">
+                <a href="https://map.kakao.com/link/map/11394059"style="text-decoration:none;color:rgb(88, 61, 48);font-weight:800" target="_blank">
+                  <span class="title" >${title}</span>
+                </a>
+              </div>
+            `;
+
+            const position = new kakao.maps.LatLng(lat, lng);
+
+            const customOverlay = new kakao.maps.CustomOverlay({
               map: map,
-              position: new kakao.maps.LatLng(el.lat, el.lng),
-              title: el.title,
+              position: position,
+              content: content,
+              yAnchor: 1,
             });
           });
         },
